3d5e14ebb21ffce725d27c858ea421669e32ab03
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-04 16:39:54 +0000

    Updated WebApps.tf

--- TF-Infra/WebApps.tf
+++ TF-Infra/WebApps.tf
@@ -1,22 +1,22 @@
-resource "azurerm_app_service_plan" "asp" {
-  name                = "webapp-asp"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  kind                = "Linux"
-  reserved            = true
+resource "azurerm_app_service_plan" "tetris_asp" {
+    name                = "multiWeb-asp"
+    resource_group_name = var.resource_group_name
+    location            = var.location
+    kind                = "Linux"
+    reserved            = true
 
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
+    sku {
+        tier     = "Standard"
+        size     = "S1"
+    }
 }
 
-resource "azurerm_app_service" "webapp" {
+resource "azurerm_app_service" "tetris_webapps" {
   count               = 3
   name                = "sktetris-${count.index}"
   location            = var.location
   resource_group_name = var.resource_group_name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+  app_service_plan_id = azurerm_app_service_plan.tetris_asp.id
 
   site_config {
     linux_fx_version = "DOCKER|skP20ContReg.azurecr.io/tetrisgameapp"
@@ -33,72 +33,13 @@ resource "azurerm_app_service" "webapp" {
   tags = local.common_tags
 }
 
-resource "azurerm_public_ip" "waf_public_ip" {
-  name                = "waf-public-ip"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
-}
+resource "azurerm_app_service_slot" "staging" {
+  count               = 3
+  name                ="staging"
+  app_service_name    = azurerm_app_service.tetris_webapps[count.index].name
+  location            = azurerm_app_service.tetris_webapps[count.index].location
+  resource_group_name= azurerm_app_service.tetris_webapps[count.index].resource_group_name
+  app_service_plan_id= azurerm_app_service_plan.tetris_asp.id
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "webapp-vnet"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  address_space       = ["10.0.0.0/16"]
-
-  subnet {
-    name           = "webapp-subnet"
-    address_prefix = "10.0.1.0/24"
-  }
-}
-
-resource "azurerm_application_gateway" "waf" {
-  name                = "tetris-waf"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  
-  sku {
-    name     = "Standard_Small"
-    tier     = "Standard"
-    capacity = 2
-  }
-
-  gateway_ip_configuration {
-    name      = "tetris-gateway-ip"
-    subnet_id = tolist(azurerm_virtual_network.vnet.subnet)[0].id
-  }
-
-  frontend_port {
-    name = "http-port"
-    port = 80
-  }
-
-  frontend_ip_configuration {
-    name                 = "tetrismy-frontend-ip"
-    public_ip_address_id = azurerm_public_ip.waf_public_ip.id
-  }
-  backend_address_pool {
-    name         = "tetris-backend-pool"
-    ip_addresses = split(",", azurerm_app_service.webapp[0].outbound_ip_addresses)
-   }
-   backend_http_settings {
-        name                  ="httpSettingName1"
-        cookie_based_affinity ="Enabled"
-        port                  ="80"
-        protocol              ="Http"
-        request_timeout       ="60"
-   }
-   http_listener {
-        name                           ="httpListenerName1"
-        frontend_ip_configuration_name ="frontendIpConfigurationName1"
-        frontend_port_name             ="frontendPortName1"
-        protocol                       ="Http"
-   }
-   request_routing_rule{
-        name                        ="ruleName1"
-        rule_type                   ="Basic"
-        http_listener_name          ="httpListenerName1"
-        backend_address_pool_name   ="backendAddressPoolName1"
-        backend_http_settings_name   ="httpSettingName1"
-   } 
+  tags = local.common_tags
 }
