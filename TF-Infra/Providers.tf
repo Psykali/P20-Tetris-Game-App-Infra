9818ecc9f64326c10c611b48522c81b492add7b4
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-05 08:15:41 +0000

    Updated Providers.tf

--- TF-Infra/Providers.tf
+++ TF-Infra/Providers.tf
@@ -7,6 +7,10 @@ terraform {
       source = "hashicorp/azurerm"
       version = "3.40.0"
     }
+     azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.71.0"
+    }
   }
 
   backend "azurerm" {
